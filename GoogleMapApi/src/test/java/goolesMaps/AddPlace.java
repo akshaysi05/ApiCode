de48bcd0fcb43ee6b259e60e1959c77ea6956445
akshaysi05 <akshaysingh8804@gmail.com>
2023-08-07 18:04:26 +0530

    Parsing the Json Response body using JsonPath class

--- GoogleMapApi/src/test/java/goolesMaps/AddPlace.java
+++ GoogleMapApi/src/test/java/goolesMaps/AddPlace.java
@@ -1,6 +1,8 @@
 package goolesMaps;
 
 import io.restassured.RestAssured;
+import io.restassured.path.json.JsonPath;
+
 import static io.restassured.RestAssured.*;
 import static org.hamcrest.Matchers.*;
 
@@ -10,11 +12,15 @@ public class AddPlace {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		RestAssured.baseURI="https://rahulshettyacademy.com";
-		given().log().all().queryParam("key", "qaclick123").headers("Content-Type","application/json")
+		String response=given().log().all().queryParam("key", "qaclick123").headers("Content-Type","application/json")
 		.body(Payload.Addplace())
 		.when().post("/maps/api/place/add/json")
 		.then().log().all().assertThat().statusCode(200).body("scope", equalTo("APP"))
-		.header("Server", equalTo("Apache/2.4.52 (Ubuntu)"));
+		.header("Server", equalTo("Apache/2.4.52 (Ubuntu)")).extract().response().asString();
+		
+		 JsonPath js= new JsonPath(response);
+		 String PlaceId=js.getString("place_id");
+		 System.out.println(PlaceId);
 
 	}
 
