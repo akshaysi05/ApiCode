ac5ba8d38053443ab2e96a33598db02a6ee809d3
akshaysi05 <akshaysingh8804@gmail.com>
2023-08-08 19:06:27 +0530

    ParseThePayloadAsExtranlJsonFilePath

--- GoogleMapApi/src/test/java/goolesMaps/AddPlace.java
+++ GoogleMapApi/src/test/java/goolesMaps/AddPlace.java
@@ -7,17 +7,21 @@ import static io.restassured.RestAssured.*;
 import static org.hamcrest.Matchers.*;
 import static org.testng.Assert.assertEquals;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
 import files.Payload;
 import files.ReusableMethods;
 public class AddPlace {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
 		RestAssured.baseURI="https://rahulshettyacademy.com";
 		JsonPath js;
 		//AddPlace
 		String response=given().log().all().queryParam("key", "qaclick123").headers("Content-Type","application/json")
-		.body(Payload.Addplace())
+		.body( new String(Files.readAllBytes(Paths.get("E:\\Downloads\\AddPlace.json"))))
 		.when().post("/maps/api/place/add/json")
 		.then().log().all().assertThat().statusCode(200).body("scope", equalTo("APP"))
 		.header("Server", equalTo("Apache/2.4.52 (Ubuntu)")).extract().response().asString();
