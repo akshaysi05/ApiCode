cfbd54f6cd7103081da5c87dd6f471688e99590e
akshaysi05 <akshaysingh8804@gmail.com>
2023-08-08 11:28:31 +0530

    GetPlaceApiAnd AsertAddressAfterUpdate

--- GoogleMapApi/src/test/java/goolesMaps/AddPlace.java
+++ GoogleMapApi/src/test/java/goolesMaps/AddPlace.java
@@ -5,6 +5,7 @@ import io.restassured.path.json.JsonPath;
 
 import static io.restassured.RestAssured.*;
 import static org.hamcrest.Matchers.*;
+import static org.testng.Assert.assertEquals;
 
 import files.Payload;
 public class AddPlace {
@@ -12,25 +13,40 @@ public class AddPlace {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		RestAssured.baseURI="https://rahulshettyacademy.com";
+		 JsonPath js;
+		//AddPlace
 		String response=given().log().all().queryParam("key", "qaclick123").headers("Content-Type","application/json")
 		.body(Payload.Addplace())
 		.when().post("/maps/api/place/add/json")
 		.then().log().all().assertThat().statusCode(200).body("scope", equalTo("APP"))
 		.header("Server", equalTo("Apache/2.4.52 (Ubuntu)")).extract().response().asString();
 		
-		 JsonPath js= new JsonPath(response);
+		  js= new JsonPath(response);
 		 String PlaceId=js.getString("place_id");
 		 System.out.println(PlaceId);
 		 
+		 //Upadte Place
+		 String NewAdd="Naurangpura police station Ahmedabad";
 		 given().log().all().queryParam("key", "qaclick123").header("Content-Type","application/json")
 		 .body("{\r\n"
 		 		+ "\"place_id\":\""+PlaceId+"\",\r\n"
-		 		+ "\"address\":\"Naurangpura police station Ahmedabad\",\r\n"
+		 		+ "\"address\":\""+NewAdd+"\",\r\n"
 		 		+ "\"key\":\"qaclick123\"\r\n"
 		 		+ "}\r\n"
 		 		+ "")
 		 .when().put("/maps/api/place/update/json")
 		 .then().log().all().assertThat().statusCode(200).body("msg", equalTo("Address successfully updated"));
+		 
+		 //GetPlace
+		String getPlaceResponse= given().log().all().queryParam("key", "qaclick123").queryParam("place_id", PlaceId)
+		 .when().get("/maps/api/place/get/json")
+		 .then().log().all().assertThat().statusCode(200).extract().response().asString();
+		js= new JsonPath(getPlaceResponse);
+		String ActualAdd=js.getString("address");
+		System.out.println(ActualAdd);
+		 assertEquals(NewAdd, ActualAdd);
+		
+		 
 
 	}
 
