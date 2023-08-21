376e24899233562a98399ef88878f10fafa749f3
akshaysi05 <akshaysingh8804@gmail.com>
2023-08-21 15:00:07 +0530

    someChangesInTimmy

--- TimmyHRMS/src/test/java/payloadTimmy/Payload.java
+++ TimmyHRMS/src/test/java/payloadTimmy/Payload.java
@@ -1,9 +1,7 @@
 package payloadTimmy;
 
 public   class Payload {
-//	int id=31;
-//	String Fromdate="2023-08-01";
-//	String ToDate="2023-08-18";
+
 	public static String  userLogsPay(int id, String fromDate ,String ToDate) {
 		
 //		return "{\r\n"
--- TimmyHRMS/src/test/java/timmyTest/UserLogs.java
+++ TimmyHRMS/src/test/java/timmyTest/UserLogs.java
@@ -5,12 +5,14 @@ import payloadTimmy.Payload;
 
 import static io.restassured.RestAssured.*;
 public class UserLogs extends Payload {
-
+	static int id=31;
+	static String Fromdate="2023-08-01";
+	static String ToDate="2023-08-18";
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		RestAssured.baseURI="http://ams.naapbooks.com:7788";
 		String Userlogs=given().log().all().header("Content-Type","application/json")
-		.body(Payload.userLogsPay(31,"2023-08-01" , "2023-08-18"))
+		.body(Payload.userLogsPay(id,Fromdate , ToDate))
 		.when().post("/Company/GetUserLog")
 		.then().log().all().assertThat().statusCode(200).extract().response().asString();
 		 JsonPath js= new JsonPath(Userlogs);
