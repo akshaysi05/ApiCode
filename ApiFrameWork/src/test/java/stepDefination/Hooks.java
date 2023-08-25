f33d829141d2e0ab3c16dc056082a52f13f283a4
akshaysi05 <akshaysingh8804@gmail.com>
2023-08-25 14:08:11 +0530

    UsingHooksClassWithoutExecutePreviousTest

---
+++ ApiFrameWork/src/test/java/stepDefination/Hooks.java
@@ -0,0 +1,22 @@
+package stepDefination;
+
+import java.io.IOException;
+
+import io.cucumber.java.Before;
+
+public class Hooks {
+	@Before("@DeletePlace")
+	public void beforeRunScenario() throws IOException {
+		//execute this code only when place_id is null
+		//write a code that will give you place_id
+		StepDefination sd= new StepDefination();
+		if(StepDefination.place_id==null) {
+		sd.add_place_pay_load_with("Golu", "Hindi", "LawGarden");
+		sd.user_calls_with_http_request("AddPlaceAPI", "POST");
+		sd.verify_place_id_created_maps_to_using("Golu", "getPlaceApi");
+		}
+		
+		
+	}
+
+}
--- ApiFrameWork/src/test/java/testRunner/TestRunner.java
+++ ApiFrameWork/src/test/java/testRunner/TestRunner.java
@@ -5,7 +5,7 @@ import org.junit.runner.RunWith;
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 @RunWith(Cucumber.class)
-@CucumberOptions(features = "src/test/java/feature",glue = {"stepDefination"},tags = "@AddPlace")
+@CucumberOptions(features = "src/test/java/feature",glue = {"stepDefination"},tags = "@DeletePlace")
 public class TestRunner {
 
 }
